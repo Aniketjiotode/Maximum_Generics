6ec6612141d69bc77d71f9ca814e7ea7eb4b849b
Aniketjiotode <aniketjiotode07@gmail.com>
2023-02-07 17:20:06 +0530

    Added: Initial code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Maximum_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
