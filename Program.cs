f696f303a851d22519ac4fe969fa5051c19368e1
Aniketjiotode <aniketjiotode07@gmail.com>
2023-02-07 19:40:04 +0530

    Added: UC_3 changes

--- Program.cs
+++ Program.cs
@@ -7,8 +7,13 @@ namespace Maximum_Generics
         static void Main(string[] args)
         {
 
-            Console.WriteLine($"Maximum number = {Test_Maximum.IntMaxnum(5, 9, 8)}");
-            Console.WriteLine($"Maximum number = {Test_Maximum.floatMaxnum(5.5f, 9.7f, 8.9f)}");
+            //Console.WriteLine($"Maximum number = {Test_Maximum.IntMaxnum(5, 9, 8)}");
+            //Console.WriteLine($"Maximum number = {Test_Maximum.floatMaxnum(5.5f, 9.7f, 8.9f)}");
+            //Console.WriteLine($"Maximum String = {Test_Maximum.stringMaxnum("A", "b", "C")}");
+            Test_Maximum <int> _Maximum = new Test_Maximum<int>();
+            Console.WriteLine($"Maximum = {_Maximum.Maximum(4,7,9)}");
+
+
         }
     }
 }
